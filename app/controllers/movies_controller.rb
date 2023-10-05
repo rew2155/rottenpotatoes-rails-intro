39a54231eaebb276bfb190ff208419a5ce3a436d
rew2155 Riley Washington <rew2155@columbia.edu>
2023-10-05 05:27:07 +0000

    it made me

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,13 +8,41 @@ class MoviesController < ApplicationController
   end
 
   def index
-      if params[:ratings] == nil
-        @ratings_to_show = Movie.all_ratings
-      else
-        @ratings_to_show = params[:ratings].keys
-        session[:ratings] = params[:ratings] 
-      end
 
+    if params[:sort].blank? && params[:ratings].blank?
+      @selected_column = 'title'
+      session[:sort] = @selected_column
+      @ratings_to_show = Movie.all_ratings
+      session[:ratings] = @ratings_to_show
+    else
+      @selected_column = params[:sort] || session[:sort]
+      session[:sort] = @selected_column
+      @ratings_to_show = params[:ratings].keys || session[:ratings] || Movie.all_ratings
+      session[:ratings] = @ratings_to_show
+      #redirect_to movies_path(sort: @selected_column);
+      redirect_to movies_path(sort: @selected_column, ratings: Hash[@ratings_to_show.map { |r| [r,1]}]) and return
+    end
+
+    @all_ratings = Movie.all_ratings
+    sort_by = @selected_column
+    @movies = Movie.with_ratings(@ratings_to_show).order(sort_by)
+    @column_css_class = {
+      "title" => "hilite bg-warning",
+      "release_date" => "hilite bg-warning"
+    }
+  end
+
+
+=begin
+  def index  
+      if params[:ratings]
+        @ratings_to_show = params[:ratings].keys
+        session[:ratings] = params[:ratings]
+      else 
+      #if params[:ratings] == nil
+        @ratings_to_show = Movie.all_ratings
+      end
+        
       @all_ratings = Movie.all_ratings
 
       sort_by = params[:sort] || 'title'
@@ -26,6 +54,7 @@ class MoviesController < ApplicationController
       }
       session[:sort] = @selected_column
   end
+=end
 
   def new
     # default: render 'new' template
