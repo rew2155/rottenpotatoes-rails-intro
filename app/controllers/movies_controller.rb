240916dc5929c102ccf8283c19e90e580174b4c8
rew2155 Riley Washington <rew2155@columbia.edu>
2023-10-05 13:10:45 +0000

    part 3 is hard

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,53 +8,27 @@ class MoviesController < ApplicationController
   end
 
   def index
-
-    if params[:sort].blank? && params[:ratings].blank?
-      @selected_column = 'title'
-      session[:sort] = @selected_column
+    if params[:ratings] == nil
       @ratings_to_show = Movie.all_ratings
-      session[:ratings] = @ratings_to_show
     else
-      @selected_column = params[:sort] || session[:sort]
-      session[:sort] = @selected_column
-      @ratings_to_show = params[:ratings].keys || session[:ratings] || Movie.all_ratings
-      session[:ratings] = @ratings_to_show
-      #redirect_to movies_path(sort: @selected_column);
-      redirect_to movies_path(sort: @selected_column, ratings: Hash[@ratings_to_show.map { |r| [r,1]}]) and return
+      @ratings_to_show = params[:ratings].keys
     end
 
+    if params[:sort] != nil
+      sort_by = params[:sort]
+      @movies = Movie.with_ratings(@ratings_to_show).order(sort_by)
+      session[:sort] = params[:sort]
+    else
+      @movies = Movie.with_ratings(@ratings_to_show)
+    end
+  
     @all_ratings = Movie.all_ratings
-    sort_by = @selected_column
-    @movies = Movie.with_ratings(@ratings_to_show).order(sort_by)
+    @selected_column = sort_by
     @column_css_class = {
       "title" => "hilite bg-warning",
       "release_date" => "hilite bg-warning"
     }
   end
-
-
-=begin
-  def index  
-      if params[:ratings]
-        @ratings_to_show = params[:ratings].keys
-        session[:ratings] = params[:ratings]
-      else 
-      #if params[:ratings] == nil
-        @ratings_to_show = Movie.all_ratings
-      end
-        
-      @all_ratings = Movie.all_ratings
-
-      sort_by = params[:sort] || 'title'
-      @movies = Movie.with_ratings(@ratings_to_show).order(sort_by)
-      @selected_column = sort_by
-      @column_css_class = {
-        "title" => "hilite bg-warning",
-        "release_date" => "hilite bg-warning"
-      }
-      session[:sort] = @selected_column
-  end
-=end
 
   def new
     # default: render 'new' template
